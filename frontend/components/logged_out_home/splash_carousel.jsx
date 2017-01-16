79ae62b088e49c8ef9fe7e01854b235fe7d2b02a
Sonik Jhang <sonik.jhang@utexas.edu>
2017-01-16 00:32:08 -0600

    carousel background

--- frontend/components/logged_out_home/splash_carousel.jsx
+++ frontend/components/logged_out_home/splash_carousel.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
 import Slider from 'react-slick';
 import {Link, hashHistory} from 'react-router';
+import UserProfile from '../user_profile/user_profile';
+import {getLocation} from '../../util/user_api_util';
 
 const Carousel = (props) => {
+  const currentUserLocation = () => {
+
+  };
+
+  const currentLocation = (pos) => {
+    let coord = pos.coords;
+    let lat = coord.latitude;
+    let lng = coord.longitude;
+    let params = `${lat},${lng}`;
+
+    const apiKey ="AIzaSyCbq38WBZIYrNol3TjlRGjL9T5dhqoefqI";
+    let url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=";
+
+    if (lat) {
+      url += params;
+      url += `&key=${apiKey}`;
+    } else {
+      return <h1>Loading</h1>;
+    }
+    getLocation(url, setLocation());
+  };
+
+  const setLocation = (data) => {
+    let address = data.results[0].formatted_address;
+    let city = address.split(', ')[1];
+    let state = address.split(', ')[2].split(" ")[0];
+  };
 
   const eventsList = () => {
     let eventList = props.eventList;
